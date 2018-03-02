abcdab22a5d0e98a81b1008397b34d655e05b83b
slorenzo <SebastianLorenzo@gmail.com>
2018-03-02 00:53:24 -0300

    Added index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,20 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        vue-mp3-to-itunes
+        mp3-to-itunes
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+      <h3 class="description">
+        A simple tool where you can convert your downloaded albums to <b>Itunes format</b>.
+      </h3>
+      <div class="upload">
+        <dropzone 
+          id="foo"
+          ref="vueDropzone"
+          :options="options"
+          :destroyDropzone="true"
+          @vdropzone-success="vsuccess"
+          @vdropzone-error="verror"
+        ></dropzone>
       </div>
     </div>
   </section>
@@ -24,10 +24,48 @@
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import Dropzone from 'nuxt-dropzone'
 
 export default {
   components: {
-    AppLogo
+    AppLogo,
+    Dropzone
+  },
+  data() {
+    return {
+      // See https://rowanwins.github.io/vue-dropzone/docs/dist/index.html#/props
+      options: {
+        url: `${(process.env.baseUrl || 'http://localhost:8080')}/api/upload`,
+        acceptedFiles: 'application/zip',
+        maxFilesize: 200, //MB
+        dictDefaultMessage: `<i class='fa fa-cloud-upload'></i> Drop your album here (.zip) to convert it.`
+      }
+    }
+  },
+  methods: {
+    vsuccess(file, response) {
+      this.success = true
+      this.$store.$toast.success('Well done! Your file will be downloaded in a few seconds', {
+        duration: 8000,
+        position: 'bottom-center'
+      })
+      this.$refs.vueDropzone.removeAllFiles()
+      this.sendConvertRequest(response.fileName)
+    },
+    verror(file, response) {
+      this.error = true
+      this.$store.$toast.error('Something is wrong')
+    },
+    async sendConvertRequest(id) {
+      const url = `${(process.env.baseUrl || 'http://localhost:8080')}/api/converter/${id}`
+      const download = `${(process.env.baseUrl || 'http://localhost:8080')}/api/converter/${id}`
+      try {
+        await this.$axios.$put(url)
+        window.location.assign(download)
+      } catch(err) {
+        this.$store.$toast.error('Something is wrong')
+      }
+    }
   }
 }
 </script>
@@ -50,15 +88,30 @@ export default {
   letter-spacing: 1px;
 }
 
-.subtitle {
+.description {
   font-weight: 300;
-  font-size: 42px;
   color: #526488;
-  word-spacing: 5px;
+  letter-spacing: 1px;
   padding-bottom: 15px;
 }
 
-.links {
+.upload {
   padding-top: 15px;
+}
+
+.dropzone {
+  min-height: 280px;
+}
+
+.dropzone .dz-message {
+  margin: 6em 0;
+}
+
+.vue-dropzone {
+  margin: 20px;
+}
+
+.dz-success-mark {
+  display: none !important;
 }
 </style>
